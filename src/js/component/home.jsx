ce902399619cfcdfc86ec51ebd228a196df9a3b3
SimoneHell <simone.hall.speaks@gmail.com>
2022-07-29 23:24:44 +0000

    complete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-function TrafficLight () {
-    return (
+function TrafficLight()
+
+{const [light, setLight] = useState("red")
+   { return (
         <div className="trafficpole">
             <div className="trafficbox">
-                <div className="redlight"></div>
-                <div className="yellowlight"></div>
-                <div className="greenlight"></div>
+                <div className={"red light " + (selectedLight === 'red' ? 'selected' : '')} onClick={() => setLight('red')}></div>
+                <div className={"yellow light " + (selectedLight === 'yellow' ? 'selected' : '')} onClick={() => setLight('yellow')}></div>
+                <div className={"green light " + (selectedLight === 'green' ? 'selected' : '')} onClick={() => setLight('green')}></div>
             </div>
         </div>
     )
+   }
+
+
 }
-
 export default TrafficLight
-
